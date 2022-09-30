832fbfeffdf69b5644f45f8f1078985aa552f7ca
Rishivanth Thulasiraman <rishithulasiramanr@gmail.com>
2022-09-30 03:05:37 -0400

    update objects.js

--- sept-30/objects.js
+++ sept-30/objects.js
@@ -53,4 +53,8 @@ class Vec {
     get length() {
         return Math.sqrt(this.x * this.x + this.y * this.y);
     }
-  }
+}
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+console.log(new Vec(3, 4).length);
