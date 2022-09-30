5a5e781cd0c858d3acbe91308c37341a141292bf
Rishivanth Thulasiraman <rishithulasiramanr@gmail.com>
2022-09-30 03:03:17 -0400

    added objects.js

---
+++ sept-30/5.js
@@ -0,0 +1,29 @@
+let capture;
+
+
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  capture = createCapture(VIDEO);
+  capture.size(width,height);
+  capture.hide();
+}
+
+function draw(){
+  background(255);
+  let pixelationLevel = 10;
+  capture.loadPixels();
+  for(let y = 0;y<capture.height;y+=pixelationLevel){
+    for(let x = 0;x<capture.width;x+=pixelationLevel){
+      let i = (y*capture.width+x)*4;
+      let r = capture.pixels[i + 0];
+      let g = capture.pixels[i + 1];
+      let b = capture.pixels[i + 2];
+  
+      let perceivedBrightness = ((0.3 * r) + (0.59 * g) + (0.11 * b));
+      let dia = map(perceivedBrightness,0,255,pixelationLevel,0);
+      fill(255,0,0);
+      noStroke();
+      ellipse(x,y,dia,dia)
+    }
+  }
+}
---
+++ sept-30/objects.js
@@ -0,0 +1,56 @@
+// A class combines related data (properties) with related actions and behaviors (methods).
+// Objects are instances of classes.
+// Eg. Math object
+// Write a class Vec that represents a vector in two-dimensional space. 
+//It takes x and y parameters (numbers), which it should save to properties of the same name.
+// Give the Vec prototype two methods, plus and minus, that take another vector as a parameter and return a new vector that has the sum or difference of the two vectors’ (this and the parameter) x and y values.
+// Add a getter property length to the prototype that computes the length of the vector — that is, the distance of the point (x, y) from the origin (0, 0).
+
+let cursor;
+function setup() {
+    createCanvas(480, 120);
+    background(204);
+    cursor = new Generator(width/2, height/2);
+}
+
+function draw() { 
+    cursor.move(map(mouseX,0,width,0,10)); 
+    cursor.display();
+}
+
+class Generator {
+
+    constructor(currentX,currentY){
+        this.x = currentX;
+        this.y = currentY;
+        this.diameter = 20;
+    }
+
+    move(speed) {
+        this.x += random(-speed, speed); 
+        this.y += random(-speed, speed);
+    };
+    
+    display() {
+        ellipse(this.x, this.y, this.diameter, this.diameter);
+    }; 
+}
+
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+  
+    plus(other) {
+        return new Vec(this.x + other.x, this.y + other.y);
+    }
+  
+    minus(other) {
+        return new Vec(this.x - other.x, this.y - other.y);
+    }
+  
+    get length() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+  }
